4c8b1df6f8aa115dd249d3c9ff2de9c23cbe719e
D065023 <david.kunz@sap.com>
2021-04-04 19:04:19 +0200

    pattern matching + orderby

--- tests/test.nim
+++ tests/test.nim
@@ -5,29 +5,44 @@ suite "selects":
   test "without columns":
     let res = nery:
       select myDbTable
-    assert res.entity.name == "myDbTable"
+    assert res.id.name == "myDbTable"
     assert res.columns == @[]
 
-  test "with columns":
+  test "columns":
     let res = nery:
       select myDbTable:
         col1
         col2
-    assert res.entity.name == "myDbTable"
-    assert res.columns == @[Id(name: "col1"),Id(name: "col2")]
+    assert res.id.name == "myDbTable"
+    assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
 
-  test "with column alias":
+  test "column alias":
     let res = nery:
       select myDbTable:
         col1 as myCol
         col2
     assert res.columns == @[Id(name: "col1", alias: "myCol"), Id(name: "col2")]
 
-  test "with table alias":
+  test "table alias":
     let res = nery:
       select myDbTable as myDb:
         col1
         col2
-    assert res.entity.alias == "myDb"
-    assert res.entity.name == "myDbTable"
+    assert res.id.alias == "myDb"
+    assert res.id.name == "myDbTable"
     assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+
+  test "orderby":
+    let res = nery:
+      select myDbTable as myDb:
+        col1
+        col2
+        orderBy:
+          col3
+          col4 asc
+          col5 desc
+    assert res.id.alias == "myDb"
+    assert res.id.name == "myDbTable"
+    assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+    assert res.orderBy == @[OrderBy(id: Id(name: "col3"), order: asc), OrderBy(
+        id: Id(name: "col4"), order: asc), OrderBy(id: Id(name: "col5"), order: desc)]
