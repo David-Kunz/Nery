5c29ceeb5b5c003f484a49043cbef99602356736
D065023 <david.kunz@sap.com>
2021-05-23 07:58:24 +0200

    semi working

--- src/nery.nim
+++ src/nery.nim
@@ -2,6 +2,8 @@ import macros
 import tables
 import fusion/matching
 
+# TODO: Use object variants and implement `==` by hand, otherwise the output is way to verbose.
+
 type
   KeyVal* = object
     key, val: string
@@ -16,15 +18,27 @@ type
   OrderBy* = object
     id*: Id
     order*: Order
+  Where* = object
+    op*: string
+    lhs*: Id
+    rhs*: Id
   Nery* = ref object
     id*: Id
     case kind*: NeryKind
     of nkSelect:
       columns*: seq[Id]
       orderBy*: seq[OrderBy]
+      where*: seq[Where]
     of nkInsert:
       entries*: Table[string, string]
 
+proc whereAnd*(query: var Nery, where: Where) =
+  if query.kind == nkSelect: # TODO: Also for update later
+    if query.where.len == 0:
+      query.where.add(where)
+    else:
+      query.where.add(Where(op: "and"))
+      query.where.add(where)
 
 proc ident2Id(ident: NimNode): Id =
   Id(name: ident.strVal)
@@ -42,39 +56,70 @@ proc command2OrderBy(command: NimNode): OrderBy =
         return
     result = OrderBy(id: Id(name: $col), order: o)
 
+proc call2Id(call: NimNode): Id =
+  if call.matches(Call[@fnName is Ident(), all @idents]):
+    result = Id(name: fnName.strVal)
+    for ident in idents:
+      if ident.matches(@ident is Ident()):
+        result.arguments.add(Id(name: ident.strVal))
+      if ident.matches(@subCall is Call()):
+        result.arguments.add(call2Id(subCall))
 
-proc infix2Id(infix: Nimnode): Id =
+proc infix2Id(infix: NimNode): Id =
   if infix.matches(Infix[@infix is Ident(), @name is Ident(),
       @alias is Ident()]):
     if infix.strVal != "as": error("Invalid infix " & infix.strVal)
     result = Id(name: name.strVal, alias: alias.strVal)
+  if infix.matches(Infix[@infix is Ident(), @call is Call(),
+      @alias is Ident()]):
+    if infix.strVal != "as": error("Invalid infix " & infix.strVal)
+    result = call2Id(call)
+    result.alias = alias.strVal
 
+proc infix2Where(infix: NimNode): Where =
+  if infix.matches(Infix[@op is Ident(), @lhs is Ident(), @rhs is Ident()]):
+    result = Where(op: op.strVal(), lhs: Id(name: lhs.strVal), rhs: Id(name: rhs.strVal))
 
 proc neryImpl(body: NimNode): Nery =
-
   result = Nery()
-
-  if body.matches(StmtList[Command[@queryKind is Ident(), @table]] | StmtList[
-      Command[@queryKind is Ident(), @table, StmtList[all @stmts]]]):
+  if body.matches(StmtList[Command[@queryKind is Ident(), @table]] |
+                  StmtList[Command[@queryKind is Ident(), @table, StmtList[all @stmts]]]):
     case queryKind.strVal:
       of "select":
         result.kind = nkSelect
-        if table.matches(@infix is Infix()):
-          result.id = infix2Id(infix)
-        if table.matches(@tableName is Ident()):
-          result.id = ident2Id(tableName)
+        if table.kind == nnkInfix:
+          result.id = infix2Id(table)
+        if table.kind == nnkIdent:
+          result.id = ident2Id(table)
         for stmt in stmts:
-          if stmt.matches(@col is Ident()):
-            result.columns.add(ident2Id(col))
-          if stmt.matches(@infix is Infix()):
-            result.columns.add(infix2Id(infix))
-          if stmt.matches(Call[@id is Ident(), @subStmtList]):
-            if id.strVal == "orderBy":
-              for subStmt in subStmtList:
-                if subStmt.matches(@col is Ident()):
-                  result.orderBy.add(ident2OrderBy(col))
-                if subStmt.matches(@command is Command()):
-                  result.orderBy.add(command2OrderBy(command))
+          if stmt.kind == nnkIdent:
+            result.columns.add(ident2Id(stmt))
+          if stmt.kind == nnkInfix:
+            result.columns.add(infix2Id(stmt))
+          if stmt.kind == nnkCall:
+            if stmt[0].strVal == "orderBy":
+              doAssert stmt.len > 1
+              for subStmt in stmt[1]:
+                if subStmt.kind == nnkIdent:
+                  result.orderBy.add(ident2OrderBy(subStmt))
+                if subStmt.kind == nnkCommand:
+                  result.orderBy.add(command2OrderBy(subStmt))
+            elif stmt[0].strVal == "where":
+              # TODO: Implement where properly (with `and` and `or`)
+              doAssert stmt.len > 1
+              for subStmt in stmt[1]:
+                if subStmt.kind == nnkInfix:
+                  result.whereAnd(infix2Where(subStmt)) 
+                if subStmt.kind == nnkPar:
+                  result.whereAnd(Where(op: "(")) 
+                  for pStmt in subStmt:
+                    if pStmt.kind == nnkInfix:
+                      result.whereAnd(infix2Where(pStmt))
+                  result.whereAnd(Where(op: ")")) 
+
+            else:
+              result.columns.add(call2Id(stmt))
+
       of "insert":
         result.kind = nkInsert
         # TODO
--- tests/test.nim
+++ tests/test.nim
@@ -2,56 +2,128 @@ import unittest
 import nery
 
 suite "select":
-  test "without columns":
-    let res = nery:
-      select myDbTable
-    assert res.id.name == "myDbTable"
-    assert res.columns == @[]
+  # test "without columns":
+  #   let res = nery:
+  #     select myDbTable
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[]
 
-  test "columns":
+  # test "columns":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+
+  # test "column alias":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1 as myCol
+  #       col2
+  #   assert res.columns == @[Id(name: "col1", alias: "myCol"), Id(name: "col2")]
+
+  # test "table alias":
+  #   let res = nery:
+  #     select myDbTable as myDb:
+  #       col1
+  #       col2
+  #   assert res.id.alias == "myDb"
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+
+  # test "orderby":
+  #   let res = nery:
+  #     select myDbTable as myDb:
+  #       col1
+  #       col2
+  #       orderBy:
+  #         col3
+  #         col4 asc
+  #         col5 desc
+  #   assert res.id.alias == "myDb"
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+  #   assert res.orderBy == @[OrderBy(id: Id(name: "col3"), order: asc), OrderBy(
+  #       id: Id(name: "col4"), order: asc), OrderBy(id: Id(name: "col5"), order: desc)]
+
+  # test "columns with functions":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       avg(col3)
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "avg", arguments: @[Id(name: "col3")])]
+
+  # test "columns with functions and alias":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       avg(col3) as myAvg
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "avg", alias: "myAvg", arguments: @[Id(name: "col3")])]
+
+  # test "columns with functions with multiple arguments":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       coalesce(col3, col4)
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "coalesce", arguments: @[Id(name: "col3"), Id(name: "col4")])]
+
+  # test "columns with nested functions":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       coalesce(col3, avg(col4))
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "coalesce", arguments: @[Id(name: "col3"), Id(name: "avg", arguments: @[Id(name: "col4")])])]
+
+  # test "columns with nested functions and alias":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       coalesce(col3, avg(col4)) as myCoalesce
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "coalesce", alias: "myCoalesce", arguments: @[Id(name: "col3"), Id(name: "avg", arguments: @[Id(name: "col4")])])]
+
+  # test "where":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       where:
+  #         col3 == col4
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+  #   assert res.where == @[Where(op: "==", lhs: Id(name: "col3"), rhs: Id(name: "col4"))]
+
+  # test "where multiple":
+  #   let res = nery:
+  #     select myDbTable:
+  #       col1
+  #       col2
+  #       where:
+  #         col3 == col4
+  #         col5 == col6
+  #   assert res.id.name == "myDbTable"
+  #   assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
+  #   assert res.where == @[Where(op: "==", lhs: Id(name: "col3"), rhs: Id(name: "col4")), Where(op: "and"), Where(op: "==", lhs: Id(name: "col5"), rhs: Id(name: "col6"))]
+
+  test "where multiple and brackets":
     let res = nery:
       select myDbTable:
         col1
         col2
+        where:
+          col3 == col4
+          (col5 == col6)
     assert res.id.name == "myDbTable"
     assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
-
-  test "column alias":
-    let res = nery:
-      select myDbTable:
-        col1 as myCol
-        col2
-    assert res.columns == @[Id(name: "col1", alias: "myCol"), Id(name: "col2")]
-
-  test "table alias":
-    let res = nery:
-      select myDbTable as myDb:
-        col1
-        col2
-    assert res.id.alias == "myDb"
-    assert res.id.name == "myDbTable"
-    assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
-
-  test "orderby":
-    let res = nery:
-      select myDbTable as myDb:
-        col1
-        col2
-        orderBy:
-          col3
-          col4 asc
-          col5 desc
-    assert res.id.alias == "myDb"
-    assert res.id.name == "myDbTable"
-    assert res.columns == @[Id(name: "col1"), Id(name: "col2")]
-    assert res.orderBy == @[OrderBy(id: Id(name: "col3"), order: asc), OrderBy(
-        id: Id(name: "col4"), order: asc), OrderBy(id: Id(name: "col5"), order: desc)]
-
-  test "function columns":
-    let res = nery:
-      select myDbTable:
-        col1
-        col2
-        avg(col3)
-    assert res.id.name == "myDbTable"
-    # assert res.columns == @[Id(name: "col1"), Id(name: "col2"), Id(name: "avg", arguments: @[Id(name: "col3")])]
+    echo res.where
+    # assert res.where == @[Where(op: "==", lhs: Id(name: "col3"), rhs: Id(name: "col4")), Where(op: "and"), Where(op: "==", lhs: Id(name: "col5"), rhs: Id(name: "col6"))]
